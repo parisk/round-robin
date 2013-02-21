a404834e011a4df7c3bf67c2762066afba85df07
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-02-21 15:34:28 +0200

    Progress...

--- src/rr.cpp
+++ src/rr.cpp
@@ -1,26 +1,48 @@
 #include <iostream>
 #include <stdlib.h>
 #include <signal.h>
+#include <time.h>
+#include <wait.h>
+#include <sys/types.h>
+#include <sys/resource.h>
 
 class RRProcess {
 	private:
 		int pid;
-		void signal ( int sig ) {
+		void sigProc ( int sig ) {
+			std::cout 	<< "SigProc: "
+						<< sig
+						<< "\n\n";
 			kill( pid, sig );
 		}
 	public:
 		RRProcess () {
 			pid = fork();
 			if ( !pid ) {
+				std::cout	<< "Process "
+							<< getpid()
+							<< " just spawned...\n";
 				raise( SIGSTOP );
-				while (1) {};
+				std::cout 	<< "Process "
+							<< getpid()
+							<< " continues...\n";
+				while (1) {
+					std::cout 	<< "\n\nProcess "
+								<< getpid()
+								<< "running...\n";
+				};
 			}
 		}
+		void activate () {
+			std::cout << "Activate me!\n";
+			sigProc( SIGCONT );
+		}
 		void exit () {
-			this -> signal( SIGSTOP );
+			std::cout << "Exit me!\n";
+			sigProc( SIGKILL );
 		}
 		int getPID () {
-			return this -> pid;
+			return pid;
 		}
 
 };
@@ -32,5 +54,8 @@ int main () {
 				<< " created process "
 				<< myProcess -> getPID()
 				<< "\n";
+	myProcess -> activate();
+	//waitpid( myProcess -> getPID(), 0, 0 );
+	sleep(2);
 	myProcess -> exit();
 }
