b39a507d77acc4bbebd9e84ccfb40536e5254b52
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-02-21 16:28:28 +0200

    First version that works

--- src/rr.cpp
+++ src/rr.cpp
@@ -6,25 +6,20 @@
 #include <sys/types.h>
 #include <sys/resource.h>
 
-void eternalLoop () {
-	while (1) {
-		std::cout 	<< "\n\nProcess "
-					<< getpid()
-					<< "running...\n";
-	};
-}
-
 class RRProcess {
 	private:
 		int pid;
 		void sigProc ( int sig ) {
-			std::cout 	<< "SigProc: "
-						<< sig
-						<< ", "
-						<< pid
-						<< "\n\n";
 			kill( pid, sig );
 		}
+		static void eternalLoop () {
+			while (1) {
+				std::cout 	<< "Process "
+							<< getpid()
+							<< "running...\n";
+			};
+		}
+
 	public:
 		RRProcess () {
 			pid = fork();
@@ -32,18 +27,16 @@ class RRProcess {
 				std::cout	<< "Process "
 							<< getpid()
 							<< " just spawned...\n";
+				eternalLoop();
 			}
 		}
 		void activate () {
-			std::cout << "Activate me!\n";
 			sigProc( SIGCONT );
 		}
 		void halt () {
-			std::cout << "Halt me!\n";
 			sigProc( SIGSTOP );
 		}
 		void exit () {
-			std::cout << "Exit me!\n";
 			sigProc( SIGKILL );
 		}
 		int getPID () {
@@ -60,10 +53,9 @@ int main () {
 				<< myProcess -> getPID()
 				<< "\n";
 	myProcess -> halt();
-	//waitpid( myProcess -> getPID(), 0, 0 );
 	sleep(2);
 	myProcess -> activate();
-	wait();
 	sleep(2);
 	myProcess -> exit();
+	std::cout	<< "\n";
 }
