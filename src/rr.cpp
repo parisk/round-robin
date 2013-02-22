a20442226810c4e9fe7f56b8913c154b148da11a
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-02-22 11:51:24 +0200

    Great progress with queue.

--- src/rr.cpp
+++ src/rr.cpp
@@ -17,25 +17,41 @@ class RRNode {
 class RRScheduler {
 	private:
 		RRNode* queue;
+		RRNode* queueTail;
+		RRNode* queueCursor;
 	
 	public:
 		RRScheduler() {
 			queue = new RRNode;
+			queueTail = queue;
 		};
 		void push ( RRProcess* proc ) {
-			/*
-			if ( queue -> proc == NULL )
-				queue -> proc = proc;
-			*/
-			queue -> proc = proc;
+			if ( queueTail -> proc == NULL )
+				queueTail -> proc = proc;
+			else { 
+				queueTail -> next = new RRNode;
+				queueTail = queueTail -> next;
+				queueTail -> proc = proc;
+			}
 		};
 		void createNewProcess () {
 			RRProcess* proc = new RRProcess();
+			proc -> halt();
 			std::cout	<< "Started process "
 						<< proc -> getPID()
 						<< "\n";
 			push( proc );
 		};
+		void drawQueue () {
+			queueCursor = queue;
+			std::cout << "Start -> ";
+			while ( queueCursor != NULL ) {
+				std::cout	<< queueCursor -> proc -> getPID()
+							<< " --> ";
+				queueCursor = queueCursor -> next;
+			}
+			std::cout << "End\n";
+		}
 		void activateNext () {
 		};
 		RRProcess* currentProcess () {
@@ -60,6 +76,9 @@ int main () {
 	std::cout	<< "Main program started with PID "
 				<< getpid()
 				<< "\n";
-	RRScheduler* scheduler = new RRScheduler();
+	RRScheduler* scheduler = new RRScheduler;
 	scheduler -> createNewProcess();
+	scheduler -> createNewProcess();
+	scheduler -> createNewProcess();
+	scheduler -> drawQueue();
 }
